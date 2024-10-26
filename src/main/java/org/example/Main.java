5d5ff6f442f3242a1a926643c45b3bf30eda62d0
Evgeniuuus <ezhelinskij@bk.ru>
2024-10-26 15:16:05 +0700

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,32 +13,32 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Gson gson = new Gson();
-        List<Visitor> Data_Visitors = null;
-        try (FileReader reader = new FileReader("C:\\JavaProjects\\Java_Stream_API23\\src\\main\\java\\org\\example\\books.json")) {
+        List<Visitor> data_Visitors = null;
+        try (FileReader reader = new FileReader("books.json")) {
             Type visitorListType = new TypeToken<List<Visitor>>() {}.getType();
-            Data_Visitors = gson.fromJson(reader, visitorListType);
+            data_Visitors = gson.fromJson(reader, visitorListType);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
-        if (Data_Visitors != null) {
+        if (data_Visitors != null) {
 // Задание 1
             System.out.println("------------------------Задание 1------------------------");
-            Data_Visitors.stream()
+            data_Visitors.stream()
                     .map(visitor -> visitor.getFirstName() + " " + visitor.getLastName())
                     .forEach(System.out::println);
-            System.out.println("Всего посетителей: " + Data_Visitors.size());
+            System.out.println("Всего посетителей: " + data_Visitors.size());
 
 // Задание 2
             System.out.println("\n------------------------Задание 2------------------------");
-            Data_Visitors.stream()
+            data_Visitors.stream()
                     .flatMap(visitor -> visitor.getFavoriteBooks().stream())
                     .distinct()
                     .sorted(Comparator.comparing(Book::getTitle))
                     .map(book -> String.format("%s, %s, %d, %s, %s",
                             book.getTitle(), book.getAuthor(), book.getYear(), book.getIsbn(), book.getPublisher()))
                     .forEach(System.out::println);
-            long uniqueBooksCount = Data_Visitors.stream()
+            long uniqueBooksCount = data_Visitors.stream()
                     .flatMap(visitor -> visitor.getFavoriteBooks().stream())
                     .distinct()
                     .count();
@@ -46,7 +46,7 @@ public class Main {
 
 // Задание 3
             System.out.println("\n------------------------Задание 3------------------------");
-            Data_Visitors.stream()
+            data_Visitors.stream()
                     .flatMap(visitor -> visitor.getFavoriteBooks().stream())
                     .distinct()
                     .sorted(Comparator.comparing(Book::getYear))
@@ -56,7 +56,7 @@ public class Main {
 
 // Задание 4
             System.out.println("\n------------------------Задание 4------------------------");
-            boolean hasAuthor = Data_Visitors.stream()
+            boolean hasAuthor = data_Visitors.stream()
                     .flatMap(visitor -> visitor.getFavoriteBooks().stream())
                     .anyMatch(book -> "Jane Austen".equalsIgnoreCase(book.getAuthor()));
             if (hasAuthor) {
@@ -67,7 +67,7 @@ public class Main {
 
 // Задание 5
             System.out.println("\n------------------------Задание 5------------------------");
-            int maxFavoriteBooks = Data_Visitors.stream()
+            int maxFavoriteBooks = data_Visitors.stream()
                     .map(visitor -> visitor.getFavoriteBooks().size())
                     .max(Comparator.naturalOrder())
                     .orElse(0);
@@ -75,10 +75,10 @@ public class Main {
 
 // Задание 6
             System.out.println("\n------------------------Задание 6------------------------");
-            double averageBooks = Data_Visitors.stream()
+            double averageBooks = data_Visitors.stream()
                     .mapToInt(visitor -> visitor.getFavoriteBooks().size())
                     .average().orElse(0);
-            Data_Visitors.stream()
+            data_Visitors.stream()
                     .filter(Visitor::isSubscribed)
                     .map(visitor -> {
                         int favoriteBooksCount = visitor.getFavoriteBooks().size();
